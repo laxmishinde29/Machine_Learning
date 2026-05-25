9f64db52a6e5b44a78593d59a98c8a959e2bf936
laxmishinde29 <laxmishinde2975@gmail.com>
2026-05-25 22:40:49 +0530

    Add files via upload

---
+++ Ball_Classification/Ball_Classification5.py
@@ -0,0 +1,41 @@
+from sklearn import tree
+
+#Rough = 1
+#Smooth = 0
+
+#Tennis = 1
+#Cricket = 2
+
+
+def main():
+    print("Ball classification case study")
+
+    #orignal encoded dataset
+    # Independent variable
+    X = [[35,1], [47, 1],[90, 0],[48, 1],[90, 0],[35,1],[92, 0],[35,1],[35,1],[35,1],[96, 0],[43, 1],[110, 0],[35, 1],[95, 0]]
+
+    # Dependent variable
+    Y = [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2]
+
+    #Independent variables for training
+    Xtrain = [[35,1], [47, 1],[90, 0],[48, 1],[90, 0],[35,1],[92, 0],[35,1],[35,1],[35,1],[96, 0],[43, 1],[110, 0]]
+
+    #Independent variables for testing
+    Xtest = [[35, 1],[95, 0]]
+
+    #Dependent variables for training
+    Ytrain = [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2]
+
+    #Dependent variables for testing
+    Ytest = [1, 2]
+
+    modelobj = tree.DecisionTreeClassifier()        #object ahe
+
+    trainedmodel = modelobj.fit(Xtrain,Ytrain)
+
+    Result = trainedmodel.predict(Xtest)          #1   2
+
+    print("Model predicts the object as :", Result)
+
+if __name__ == "__main__":
+    main()
