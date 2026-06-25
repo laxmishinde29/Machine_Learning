5c36ff4eeae39741e7b9984e260c0c195e4dfd05
laxmishinde29 <laxmishinde2975@gmail.com>
2026-06-25 10:03:43 +0530

    Add files via upload

---
+++ Iris_Classification/CaseStudyStep1.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay
+)
+
+Border = "-"*40
+##############################################################
+# Step 1: Load the Dataset
+##############################################################
+
+print(Border)
+print("Step 1 : Load the dataset")
+print(Border)
+
+DatasetPath = "iris.csv"
+
+df = pd.read_csv(DatasetPath)
+
+print("Dataset gets loaded succesfully...")
+print("Initial entries from dataset :")
+print(df.head())
---
+++ Iris_Classification/CaseStudyStep4.py
@@ -0,0 +1,97 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay
+)
+
+Border = "-"*40
+##############################################################
+# Step 1: Load the Dataset
+##############################################################
+
+print(Border)
+print("Step 1 : Load the dataset")
+print(Border)
+
+DatasetPath = "iris.csv"
+
+df = pd.read_csv(DatasetPath)       #dataframe
+
+print("Dataset gets loaded succesfully...")
+print("Initial entries from dataset :")
+print(df.head())
+
+##############################################################
+# Step 2: Data Anylysis(EDA Exploritory data analysis)
+##############################################################
+
+print(Border)
+print("Step 2 : Data Analysis")
+print(Border)
+
+print("Shape of Dataset :",df.shape)
+print("Column names :",list(df.columns))
+
+print("Missing values (per column)")
+print(df.isnull().sum())
+
+print("Class Distribution (Species count)")
+print(df["species"].value_counts())
+
+print("Statistical Report of dataset")
+print(df.describe())
+
+##############################################################
+# Step 3: Decide Independent and dependent variables
+##############################################################
+
+print(Border)
+print("Step 3: Decide Independent and dependent variables")
+print(Border)
+
+#X : Independent Variables / Feature
+#Y : Dependent Variables / Lables
+
+feature_cols = [
+    "sepal length (cm)",
+    "sepal width (cm)",
+    "petal length (cm)",
+    "petal width (cm)"
+]
+
+X = df[feature_cols]
+Y = df["species"]
+
+print("X shape :",X.shape)
+print("Y shape :",Y.shape)
+
+##############################################################
+# Step 4: Visualization of dataset
+##############################################################
+
+print(Border)
+print(" Step 4: Visualization of dataset")
+print(Border)
+
+#Scatter plot
+
+plt.figure(figsize=(7,5))
+
+for sp in df["species"].unique():
+    temp = df[df["species"] == sp]
+    plt.scatter(temp["petal length (cm)"], temp["petal width (cm)"], label = sp)
+
+plt.title("Iris : Petal length vs Petal width")
+
+plt.xlabel("petal length (cm)")
+plt.ylabel("petal width (cm)")
+
+plt.legend()
+plt.grid(True)
+plt.show()
