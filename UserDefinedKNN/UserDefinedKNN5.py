f2115e1756fe9b3c9f863c2ee03e48f480cd99b8
laxmishinde29 <laxmishinde2975@gmail.com>
2026-07-20 19:20:53 +0530

    Add files via upload

---
+++ UserDefinedKNN/UserDefinedKNN5.py
@@ -0,0 +1,91 @@
+#  [A,B,C,D]
+#X [1,2,3,5]
+#Y [2,3,1,6]
+#  [R,R,B,B]
+
+#predict(3,3) -> ?
+
+import numpy as np 
+import math
+
+def EucDistance(P1,P2):
+    Ans = math.sqrt((P1['X'] - P2['X']) ** 2 + (P1['Y'] - P2['Y']) **2)
+
+    return Ans 
+
+def MarvellousKNeighborsClassifier():
+    Border = "-"*40
+    data = [
+        {'point' : 'A','X' : 1, 'Y' : 2, 'label' : 'Red'},
+        {'point' : 'B','X' : 2, 'Y' : 3, 'label' : 'Red'},
+        {'point' : 'C','X' : 3, 'Y' : 1, 'label' : 'Blue'},
+        {'point' : 'D','X' : 5, 'Y' : 6, 'label' : 'Blue'}
+    ]
+
+    print(Border)
+    print("Marvellous UserDefined KNN")
+    print(Border)
+
+    print(Border)
+    print("Training dataset")
+    print(Border)
+
+    for i in data:
+        print(i)
+
+    print(Border)
+
+    new_point = {'X' : 3, 'Y' : 3}
+
+    #calculate all distances
+
+    for d in data:
+        d['distance'] = EucDistance(d, new_point)
+
+    print(Border)
+    print("Clculated distances are :")
+    print(Border)
+
+    for d in data:
+        print(d)
+
+    sorted_data = sorted(data, key=lambda item :item['distance'])
+
+    print(Border)
+    print("Sorted data is :")
+    print(Border)
+
+    for d in sorted_data:
+        print(d)
+
+    k = 3
+    nearest = sorted_data[:k]
+
+    print(Border)
+    print("Nearest three elements are :")
+    print(Border)
+
+    for d in nearest:
+        print(d)
+
+    #Voting
+
+    votes = {}
+    for neighbour in nearest:
+        label = neighbour['label']
+        votes[label] = votes.get(label,0) + 1
+
+    print(Border)
+    print("Voting result is :")
+    print(Border)
+
+    for d in votes:
+        print("Name :",d,"Number of votes :",votes[d])
+
+    print(Border)
+
+def main():
+    MarvellousKNeighborsClassifier()
+
+if __name__ == "__main__":
+    main()
